4de3e476f5bd83e9c0c2cff969aa5791cc85263b
mehta-preyansh <preyanshmehtaben@gmail.com>
2023-05-25 18:04:23 +0530

    Added score and resetting

--- game.js
+++ game.js
@@ -1,5 +1,15 @@
+//canvas size and dom node
 const board = document.getElementById("board");
-let boardSize = 18;
+let boardSize = 22;
+
+//snake head motion
+let pause = false;
+
+//Game reset button
+
+
+//score element
+let score = document.getElementById("score");
 
 //Adding snake and food markup in grid.
 let snake = document.createElement("div");
@@ -19,17 +29,21 @@ let xVelocity = 0;
 let yVelocity = 0;
 
 //snake initial position
-let snakeX=0;
-let snakeY=0;
+let snakeX=1;
+let snakeY=1;
 
 //foood initialization
 let foodX=0;
 let foodY=0;
 
+//initial score
+let count=0;
+let high=0;
+
 //Placing snake head in grid initially.
 function placeSnake(){
   for(let i=0; i<snakeBody.length; i++){
-    snakeBody[i][0].remove();
+    snakeBody[i].remove();
   }
   snakeBody = [];
   snakeX=Math.floor(Math.random()*boardSize)+1;
@@ -43,9 +57,18 @@ function placeSnake(){
 function placeFood(){
   foodX = Math.floor(Math.random()*boardSize)+1;
   foodY = Math.floor(Math.random()*boardSize)+1;
-  food.style.gridRowStart=foodY;
-  food.style.gridColumnStart=foodX;
-  food.classList.add("food");
+  let flag = false;
+  for(let i=0; i<snakeBody.length; i++){
+    if(snakeBody[i].style.gridRowStart==foodY && snakeBody[i].style.gridColumnStart==foodX){
+      flag = true;
+      placeFood();
+    }
+  }
+  if(flag==false){
+    food.style.gridRowStart=foodY;
+    food.style.gridColumnStart=foodX;
+    food.classList.add("food");
+  }
 }
 
 //Changing snake direction
@@ -77,29 +100,32 @@ function changeDirection(e){
       break;
     }
 }
-//snake head motion
-function moveSnake(){
-  for(let i=snakeBody.length-1; i>0; i--){
-    snakeBody[i][0].style.gridRowStart=snakeBody[i-1][0].style.gridRowStart;
-    snakeBody[i][0].style.gridColumnStart=snakeBody[i-1][0].style.gridColumnStart;
-  }
-  if(snakeBody.length){
-    snakeBody[0][0].style.gridRowStart=snakeY;
-    snakeBody[0][0].style.gridColumnStart=snakeX;
-  }
-  snakeX+=xVelocity;
-  snakeY+=yVelocity;
 
-  snake.style.gridRowStart=snakeY;
-  snake.style.gridColumnStart=snakeX;
+function moveSnake(){
+  if(!pause){
+    for(let i=snakeBody.length-1; i>0; i--){
+      snakeBody[i].style.gridRowStart=snakeBody[i-1].style.gridRowStart;
+      snakeBody[i].style.gridColumnStart=snakeBody[i-1].style.gridColumnStart;
+    }
+    if(snakeBody.length){
+      snakeBody[0].style.gridRowStart=snakeY;
+      snakeBody[0].style.gridColumnStart=snakeX;
+    }
+    snakeX+=xVelocity;
+    snakeY+=yVelocity;
+  
+    snake.style.gridRowStart=snakeY;
+    snake.style.gridColumnStart=snakeX;
+  }
+  
   for(let i=0; i<snakeBody.length; i++){
-    if(snakeY==snakeBody[i][0].style.gridRowStart && snakeX==snakeBody[i][0].style.gridColumnStart){
-      alert("Game over");
+    if(snakeY==snakeBody[i].style.gridRowStart && snakeX==snakeBody[i].style.gridColumnStart){
+      pause=true;
       reset();
     }
   }
   if(snakeX>boardSize || snakeX<1 || snakeY >boardSize || snakeY <1){
-    alert("Game over")
+    pause=true;
     reset();
   }
 }
@@ -111,23 +137,32 @@ function grow(){
   newSegment.style.gridRowStart=foodY;
   newSegment.style.gridColumnStart=foodX;
   board.appendChild(newSegment);
-  snakeBody.push([newSegment, {x: foodX, y: foodY}]);
-  console.log(snakeBody)
+  snakeBody.push(newSegment);
+  
+  count++;
+  if(count>high){
+    high++;
+  }
+  score.innerHTML=`Score: ${count} High score: ${high}`;
 }
 
 //reset game
 function reset(){
+  count=0;
   speed = 10;
   xVelocity = 0;
   yVelocity = 0;
-  placeFood();
-  placeSnake();
+  // placeSnake();
+  // placeFood();
+  score.innerHTML=`Score: ${count} High score: ${high}`
 }
 
 //Opening screen.
 window.onload= function(){
   placeSnake();
   placeFood();
+  high=0;
+  score.innerHTML=`Score: ${count} High score: ${high}`;
 }
 
 //Game Loop 
